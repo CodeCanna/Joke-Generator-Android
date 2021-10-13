462cd10411dfec072a30180531373abe8dd0fb6e
CodeCanna <codecannamw@gmail.com>
2021-10-13 10:58:15 -0600

    Removed two TODOs that are done.

--- app/src/main/java/io/codecanna/jokegenerator/MainActivity.java
+++ app/src/main/java/io/codecanna/jokegenerator/MainActivity.java
@@ -1,7 +1,5 @@
 package io.codecanna.jokegenerator;
 
-// TODO: Add a joke drum sound that is played after each button press.
-// TODO: Style the joke display and increase joke font size.
 // TODO: Add a thumbs up and thumbs down buttons so users can rate the joke.
 // TODO: Maybe even a share button?
 
